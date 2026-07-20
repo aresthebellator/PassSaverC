f635bc1105198f2dd42efd29857dcdf9e50382cf
ares <andreacreati00@gmail.com>
2026-07-20 23:59:01 +0000

    Add header comments to PassSaver.c

--- PassSaver.c
+++ PassSaver.c
@@ -1,3 +1,10 @@
+// ===============================================
+// PassSaverC
+// Copyright (c) 2026 aresthebellator
+// Version: 1.0 
+// ===============================================
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
